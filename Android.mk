c4039d8515c21744538168f3c603b1e8e079c787
Adrien Bioteau <adrien.bioteau@gmail.com>
2018-07-06 10:48:05 +0200

    Fix missing build.prop file copy

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ ifneq ("$(wildcard $(PRODUCT_OUT)/odm.img)","")
 endif
 
 # Copy all files in order to rebuild ODM image with command "make custom_images"
-$(shell cp $(LOCAL_PATH)/odm_version.prop $(TARGET_OUT_ODM)/. > /dev/null)
+$(shell cp $(LOCAL_PATH)/build.prop $(TARGET_OUT_ODM)/. > /dev/null)
 $(shell mkdir -p $(TARGET_OUT_ODM)/bin)
 $(foreach p,$(wildcard $(LOCAL_PATH)/bin/*),$(shell cp -r $(LOCAL_PATH)/bin/$(notdir $(p)) $(TARGET_OUT_ODM)/bin/. > /dev/null))
 $(shell mkdir -p $(TARGET_OUT_ODM)/firmware)
